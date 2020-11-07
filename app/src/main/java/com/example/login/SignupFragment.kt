086dae62f658719039bf69ef95aa07d19567130d
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-11-07 20:13:44 -0300

    add layout Register

--- app/src/main/java/com/example/login/SignupFragment.kt
+++ app/src/main/java/com/example/login/SignupFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.Navigation
 
 
 class SignupFragment : Fragment() {
@@ -15,6 +17,16 @@ class SignupFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_signup, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<ImageView>(R.id.img_back_signup).setOnClickListener {
+            navController.navigate(R.id.action_signupFragment2_to_loginFragment)
+        }
     }
 
 }
