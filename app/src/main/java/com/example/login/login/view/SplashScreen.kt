79bf15ebffe9bb5b8005da033c2e6d408153468c
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-11-13 15:39:56 -0300

    animacao de splashScreen

--- app/src/main/java/com/example/login/login/view/SplashScreen.kt
+++ app/src/main/java/com/example/login/login/view/SplashScreen.kt
@@ -1,10 +1,12 @@
 package com.example.login.login.view
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.ImageView
 import com.example.login.R
 
 class SplashScreen : AppCompatActivity() {
@@ -12,8 +14,26 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        animation()
+
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }, 3000)
+    }
+
+    fun animation() {
+        val image = findViewById<ImageView>(R.id.img_splash)
+        image.animate().apply {
+            duration = 2500
+            scaleX(1.05f)
+            scaleY(1.05f)
+        }.withEndAction{
+            image.animate().apply {
+                duration = 500
+                alpha(0f)
+            }
+        }
+
     }
 }
